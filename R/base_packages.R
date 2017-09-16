832e1c1f7509a751c9f1b6bbad842c9560db2265
peterhurford <peter@peterhurford.com>
2017-09-16 00:43:30 -0500

    Update base package list

--- R/base_packages.R
+++ R/base_packages.R
@@ -1,4 +1,6 @@
 #' A list of all the base packages.
 #' This is useful for knowing what not to try to install or uninstall.
+#'
+#' From https://github.com/wch/r-source/tree/trunk/src/library
 #' @export
-base_packages <- function() { c("stats", "graphics", "grDevices", "utils", "datasets", "methods", "base", "tools", "utils") }
+base_packages <- function() { c("stats", "graphics", "grDevices", "utils", "datasets", "methods", "base", "tools", "utils", "compiler", "grid", "parallel", "profile", "splines", "stats4", "tcltk", "translations", "lattice", "Matrix") }
